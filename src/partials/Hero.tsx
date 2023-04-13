63fd1672edfcc8c8477d10596c599fc22e4bc24c
John Simiyu <jonnyeinstein@gmail.com>
2023-04-14 02:21:13 +0300

    portfolio template

---
+++ src/partials/Hero.tsx
@@ -0,0 +1,64 @@
+import {
+  GradientText,
+  HeroAvatar,
+  HeroSocial,
+  Section,
+} from 'astro-boilerplate-components';
+
+const Hero = () => (
+  <Section>
+    <HeroAvatar
+      title={
+        <>
+          Hi there, I'm <GradientText>Lord Matrix</GradientText> 👋
+        </>
+      }
+      description={
+        <>
+          I am a full-stack developer with a major specialization in backend
+          development. I am proud to hold a First Class Honors degree in BSc
+          Telecommunications and IT from Kenyatta University, where I gained a
+          strong foundation in computer science and software engineering..
+        </>
+      }
+      avatar={
+        <img
+          className="h-80 w-64"
+          src="/assets/images/avatar.svg"
+          alt="Avatar image"
+          loading="lazy"
+        />
+      }
+      socialButtons={
+        <>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/twitter-icon.png"
+              alt="Twitter icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/facebook-icon.png"
+              alt="Facebook icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/linkedin-icon.png"
+              alt="Linkedin icon"
+            />
+          </a>
+          <a href="/">
+            <HeroSocial
+              src="/assets/images/youtube-icon.png"
+              alt="Youtube icon"
+            />
+          </a>
+        </>
+      }
+    />
+  </Section>
+);
+
+export { Hero };
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,9 @@
+export const AppConfig = {
+  site_name: 'John Simiyu',
+  title: 'John Simiyu',
+  description:
+    'John Simiyu is a software engineer and a blogger. He writes about software development, programming, and technology.',
+  author: 'John Simiyu',
+  locale_region: 'en-us',
+  locale: 'en',
+};
